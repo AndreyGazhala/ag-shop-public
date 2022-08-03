05c86599c1bfed0962683ae97f79446b283364d1
Andrey Gazhala <andreygazhala@gmail.com>
2022-08-04 00:21:32 +0300

    Changed greetings text

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 80
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+  res.end('<h1>Welcome to Andrey Gazhala\'s shop.</h1>')
 })
 
 server.listen(port, () => {
